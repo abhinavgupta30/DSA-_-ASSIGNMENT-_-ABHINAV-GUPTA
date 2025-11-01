94249bddd72e292806f6e5717f851d12fdac2a5f
abhinavgupta30 <abhiguptaa41@gmail.com>
2025-11-01 20:46:10 +0530

    Update DSA _ Q1 _ ARRAYS.cpp

--- DSA _ Q1 _ ARRAYS.cpp
+++ DSA _ Q1 _ ARRAYS.cpp
@@ -1,57 +1,43 @@
 #include <iostream>   
 using namespace std;
-
-class TwoSum {
+class TwoSum 
+{
 public:
-    // Function to find indices of two numbers that add up to the target
-    void findIndices(int nums[], int n, int target) {
+    void findIndices(int nums[], int n, int target) 
+{
+        for (int i = 0; i < n; i++)
+            {
+            for (int j = i + 1; j < n; j++) 
+            {
 
-        // Outer loop goes through each element one by one
-        for (int i = 0; i < n; i++) {
-
-            // Inner loop checks the element after the current one
-            for (int j = i + 1; j < n; j++) {
-
-                // Check if the sum of nums[i] and nums[j] is equal to target
-                if (nums[i] + nums[j] == target) {
-
-                    // If yes, print the indices and the numbers
+                // Check if the sum of nums[i] and nums[j] is equal to target (example in the the question 1st and 2nd indices are 2 and 7 = 9(target value))
+                if (nums[i] + nums[j] == target)
+                {
                     cout << "Indices: [" << i << ", " << j << "]" << endl;
-                    cout << "Numbers: " << nums[i] << " + " << nums[j]
-                         << " = " << target << endl;
+                    cout << "Numbers: " << nums[i] << " + " << nums[j]    << " = " << target << endl;
                     return;
                 }
             }
         }
 
-        // If no pair found write this
         cout << "No pair found that adds up to target!" << endl;
     }
 };
 
-int main() {
-
-    // Step 1: Declare and initialize the array
+int main()
+{
     int nums[] = {2, 7, 11, 15};  // Given array of numbers from the question
-
-    // Step 2: Store the target value we need to reach
     int target = 9;  // Target sum value
-
-    // Step 3: Find size of the array
-    int n = 4; 
-
-    // Step 4: Create an object of the TwoSum class
+    int n = 4;
     TwoSum obj;
-
-    // Step 5: Print the array and target for clarity
     cout << "Array elements: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << nums[i] << " ";
     }
     cout << "\nTarget value: " << target << endl << endl;
-
-    // Step 6: Call the class function to find indices
     obj.findIndices(nums, n, target);
     return 0;
 }
+
 
